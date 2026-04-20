48b279d35fb7a266909197f3d20d6eb33d0fdd0f
Gianni <BussolettiGianni@gmail.com>
2026-04-20 15:56:57 +0200

    Video section

--- homepage.js
+++ homepage.js
@@ -15,15 +15,33 @@ for (let i = 0; i < 8; i++) {
 // Inject carosello
 
 const carouselPerTe = document.getElementById("per-te-carousel")
+const artistiCarousel = document.getElementById("artisti-carousel")
 
 for (let i = 0; i < 15; i++) {
   carouselPerTe.innerHTML += `
 
-            <div style="max-width:150px;" class="card border-0 p-2">
+            <div style="max-width:150px;" class="card border-0 px-2">
               <img src="https://cdn-images.dzcdn.net/images/cover/e16455433a84c7e19025403ae3eec52d/250x250-000000-80-0-0.jpg" class="card-img-top w-100 rounded-1" alt="..." />
               <div class="card-body p-0 pt-2">
                 <p style="font-size:0.8em;" class="card-text">
                   Best of queen, Jimi Hendrix, Edith Piaf
+                </p>
+              </div>
+            </div>
+
+          `
+  //
+  //
+  //
+  //
+  artistiCarousel.innerHTML += `
+
+            <div style="max-width:150px;" class="card border-0 px-2">
+              <img src="https://cdn-images.dzcdn.net/images/artist/7dc65523befdb7b865eb3f8595bbbd40/500x500-000000-80-0-0.jpg" class="card-img-top w-100 rounded-circle" alt="..." />
+              <div class="card-body p-0 pt-2">
+              <h6 style="font-size:.9em;" class="card-title pb-0 mb-1">Willie Peyote</h6>
+                <p style="font-size:0.8em;" class="card-text">
+                  Artista
                 </p>
               </div>
             </div>
@@ -34,20 +52,20 @@ for (let i = 0; i < 15; i++) {
 const rightMovement = function (event) {
   if (event === 0) {
     carouselPerTe.scrollBy(consigliati.offsetWidth, 0)
+  } else if (event === 1) {
+    artistiCarousel.scrollBy(artistiCarousel.offsetWidth, 0)
   }
-  //   else if (event === 1) {
-  //     watchAgainElementId.scrollBy(watchAgainElementId.offsetWidth, 0)
-  //   } else {
+  // else {
   //     newReleasesElementId.scrollBy(watchAgainElementId.offsetWidth, 0)
   //   }
 }
 const leftMovement = function (event) {
   if (event === 0) {
     carouselPerTe.scrollBy(-consigliati.offsetWidth, 0)
+  } else if (event === 1) {
+    artistiCarousel.scrollBy(-artistiCarousel.offsetWidth, 0)
   }
-  //   else if (event === 1) {
-  //     watchAgainElementId.scrollBy(-watchAgainElementId.offsetWidth, 0)
-  //   } else {
+  //  else {
   //     newReleasesElementId.scrollBy(-watchAgainElementId.offsetWidth, 0)
   //   }
 }
