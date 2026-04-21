38235595416e0e6135c1692c723ed8baa4d8ed24
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-21 10:43:30 +0200

    carousel

--- carousel.js
+++ carousel.js
@@ -1,5 +1,6 @@
-const searchWorld =
-  "https://striveschool-api.herokuapp.com/api/deezer/search?q=avril";
+const searchGeneries =
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=genres";
+// arrayparolericerca${input.value}
 const albumDetails =
   "https://striveschool-api.herokuapp.com/api/deezer/album/75621062";
 const artistDetails =
@@ -8,7 +9,7 @@ const artistDetails =
 const carouselRow = document.getElementById("carouselRow");
 
 const getAlbum = function () {
-  fetch(searchWorld)
+  fetch(searchGeneries)
     .then((res) => {
       if (res.ok) {
         return res.json();
@@ -18,6 +19,28 @@ const getAlbum = function () {
     })
     .then((data) => {
       console.log(data);
+      data.data.forEach((track) => {
+        const id = track.id;
+        console.log(id);
+        const titoloAlbum = track.title;
+        const imgAlbum = track.album.cover_medium;
+        const artistaAlbum = track.artist.name;
+        console.log(titoloAlbum, imgAlbum, artistaAlbum);
+        const rowCarosello = document.getElementById("carouselRow");
+        const cardCarosello = document.createElement("div");
+        cardCarosello.classList.add(
+          "card",
+          "col-6",
+          "m-3",
+          "position-relative",
+        );
+        cardCarosello.innerHTML = `<img src="${imgAlbum}" alt="Preferiti Spotify" class="img-fluid rounded-1">
+                       <div class="card-body">
+                       <p class="card-text">${titoloAlbum} • ${artistaAlbum}</p>
+                       <a href="#" class="btn text-black rounded-circle position-absolute" style="bottom:35%;right: 10%; background-color:#3BE477" ><i class="bi bi-play-fill"></i></a>
+                       </div>`;
+        rowCarosello.appendChild(cardCarosello);
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -32,41 +55,7 @@ getAlbum();
 //             <div class="carousel-item active">
 //                  <div class="container-fluid">
 //                     <div class="row" id="carouselRow">
-//                        <div class="card col-6">
-//                        <img src="https://www.placecats.com/100/100" alt="Preferiti Spotify" class="img-fluid rounded-1">
-//                        <div class="card-body">
-//                        <p class="card-text">Brano1</p>
-//                        <a href="#" class="btn btn-success text-black"></a>
-//                        </div>
-//                        </div>
-//                        <div class="card col-6">
-//                        <img src="https://www.placecats.com/100/100" alt="Preferiti Spotify" class="img-fluid rounded-1">
-//                        <div class="card-body">
-//                        <p class="card-text">Brano2</p>
-//                        <a href="#" class="btn btn-success text-black"></a>
-//                        </div>
-//                        </div>
 //                     </div>
-//                 </div>
-//             </div>
-//             <!-- seconda slide -->
-//                <div class="carousel-item active">
-//                  <div class="container-fluid">
-//                     <div class="row">
-//                        <div class="card col-6">
-//                        <img src="https://www.placecats.com/100/100" alt="Preferiti Spotify" class="img-fluid rounded-1">
-//                        <div class="card-body">
-//                        <p class="card-text">Brano3</p>
-//                        <a href="#" class="btn btn-success text-black"></a>
-//                        </div>
-//                        </div>
-//                        <div class="card col-6">
-//                        <img src="https://www.placecats.com/100/100" alt="Preferiti Spotify" class="img-fluid rounded-1">
-//                        <div class="card-body">
-//                        <p class="card-text">Brano</p>
-//                        <a href="#" class="btn btn-success text-black"></a>
-//                        </div>
-//                        </div>
 //                     </div>
 //                 </div>
 //             </div>
