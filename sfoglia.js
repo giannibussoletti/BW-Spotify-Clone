9349833905904e6c1ae1701c2e59cd090e6b92e6
cafagnarob <cafagna.rob@gmail.com>
2026-04-23 02:18:55 +0200

    piccoli fix

--- sfoglia.js
+++ sfoglia.js
@@ -1,10 +1,11 @@
-const query = "rock"
+const query = "rap"
 const searchWorld = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`
 // // arrayparolericerca${input.value}
 // const albumDetails = "https://striveschool-api.herokuapp.com/api/deezer/album/75621062"
 // const artistDetails = "https://striveschool-api.herokuapp.com/api/deezer/album/75621062"
 
 const cardSfoglia = document.querySelectorAll(".card-sfoglia")
+console.log(cardSfoglia)
 
 const cardContainer = document.getElementById("card-container")
 // funzione estrazione random color
@@ -28,7 +29,6 @@ fetch(searchWorld)
     for (let i = 0; i < data.data.length; i++) {
       const nomeArtisti = data.data[i].artist.name
       const fotoArtisti = data.data[i].artist.picture_medium
-      const idArtisti = data.data[i].artist.id
 
       cardContainer.classList.add("d-flex")
       const card = document.createElement("div")
@@ -45,21 +45,19 @@ fetch(searchWorld)
 
       card.style.overflow = "clip"
       card.innerHTML = `
-      <a class="text-white text-decoration-none" href="./artist_page.html?id=${idArtisti}"><div class="d-flex rounded-4 h-100 position-relative" style="overflow:clip; background-color:${randomColor}; min-height:140px;">
-      <h3 style="z-index:10" class="p-3 fw-bold" id="name-singer">${nomeArtisti}</h3>
+      <div class="d-flex rounded-4 h-100" style="overflow:clip; background-color:${randomColor};">
+      <h3 class="p-3 fw-bold" id="name-singer">${nomeArtisti}</h3>
                 <img
                   src=${fotoArtisti}
-                  class="rounded-4"
+                  class="rounded-4 w-50"
                   alt="foto_artista"
                   style="
                     rotate: 30deg;
-                    position: absolute;
-                    bottom:-30px;
-                    right:-10px;
-                    width:130px;
-                    height:130px;
+                    position: relative;
+                    top: 32px;
+                    right: -10px;
                   "
-                /></div></a>`
+                /></div>`
 
       cardContainer.appendChild(card)
     }
@@ -68,11 +66,11 @@ fetch(searchWorld)
     console.log("Errore", err)
   })
 
-// document.addEventListener("click", function (e) {
-//   const card = e.target.closest(".card-sfoglia")
-//   if (card) {
-//     const artistName = card.querySelector("#name-singer").innerText
-//     window.location.href = `artist_page.html?id=${artistName}`
-//     // Nota: se passi il nome, il mio artist_page.js lo gestirà tramite la search
-//   }
-// })
+document.addEventListener("click", function (e) {
+  const card = e.target.closest(".card-sfoglia")
+  if (card) {
+    const artistName = card.querySelector("#name-singer").innerText
+    window.location.href = `artist_page.html?id=${artistName}`
+    // Nota: se passi il nome, il mio artist_page.js lo gestirà tramite la search
+  }
+})
